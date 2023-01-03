1ee87051599672ba685203e3fdcd9dd9dd6bcd49
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-01-02 21:31:09 -0300

    Criando carrossel

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from '..';
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -12,10 +11,10 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7475d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
+  minWidth: 540,
 
   display: 'flex',
   alignItems: 'center',
