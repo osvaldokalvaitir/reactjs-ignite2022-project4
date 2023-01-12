01fbd81cb8870afbb3aca69728386afc0f4af42d
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-01-11 21:37:50 -0300

    Estrutura de sucesso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react';
@@ -30,19 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product
-            href={`/product/${product.id}`}
-            key={product.id}
-            className="keen-slider__slide"
-            prefetch={false}
-          >
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-    
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+      
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )})}
     </HomeContainer>
   )
